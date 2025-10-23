6c5a94fcf94cf2fdebeda29c746fba34f8afff57
Sandu Bogdan <bogdanelsandu@hotmail.com>
2025-10-23 20:41:30 +0300

    .

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 try {
-    & scoop list 1>$NULL -ErrorAction Stop
+    scoop list 1>$NULL
     echo "Scoop already installed."
 }
 catch {
@@ -50,15 +50,17 @@ reg export "`"$regPath`"" "`"$exportFile`"" /y
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Exported fileexts.cso key to $exportFile in case anything goes wrong."
 } else {
-    Write-Error "Export failed (code $LASTEXITCODE)"
+    Write-Host "Export failed (code $LASTEXITCODE)"
 }
 
 mkdir -Force "C:\Program Files\ISOCONVERT"
 
 echo "Automatic setup completed. Please do the following to complete the installation:"
 echo "1. Copy and paste all the executables from ./output into the C:\Program Files\ISOCONVERT folder we made for you"
-echo "2. Consecutivelly double-click on all the .reg (registry) files in the local directory to manually load them"
-echo "3. Use the tool to make a .iso and .cso, and right click on the folder, .iso and .cso files to make sure the utility is showing up correctly"
-echo "4. Once you're done, come back and press any key to exit"
+echo "2. Manually install maxcso. You can find binaries at https://github.com/unknownbrackets/maxcso/releases/tag/v1.13.0 (unzip the archives and add the output directory to path; if you are confused on the process, feel free to ask on our github repo: https://github.com/cornusandu/ISOConvert/issues)"
+echo "3. Consecutivelly double-click on all the .reg (registry) files in the local directory to manually load them"
+echo "4. Use the tool to make a .iso and .cso, and right click on the folder, .iso and .cso files to make sure the utility is showing up correctly"
+echo "|  Note: If you're on Windows 11, you may need to click on ;'Show more options' for the utility to appear"
+echo "5. Once you're done, come back and press any key to exit"
 
 pause
